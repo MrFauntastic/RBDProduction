5aa8d92029a5262ce9fd81f0abf81d66119018c5
MrFauntastic <116427908+MrFauntastic@users.noreply.github.com>
2022-10-22 17:50:34 +0300

    Delete js directory

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector("nav")
-const menu = navbar.querySelector("ul")
-const openBtn = navbar.querySelector(".fa-bars")
-const closeBtn = navbar.querySelector(".fa-times")
-
-
-openBtn.onclick = () => menu.classList.toggle("menu")
-closeBtn.onclick = () => menu.classList.toggle("menu")
